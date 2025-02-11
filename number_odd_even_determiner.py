0e6ff83b77c4ff3984e8243575e96bd218996318
+ <+>
2025-02-11 10:50:07 +0300

    Created a number determiner

---
+++ number_odd_even_determiner.py
@@ -0,0 +1,9 @@
+def numbers(number):
+    if number % 2 == 0:
+        print(f"{number} is Even")
+    else:
+        print(f"{number} is Odd")
+        
+num = input("Enter number: ")
+num = int(number)
+numbers(num) 
